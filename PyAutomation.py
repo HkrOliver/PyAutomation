ceb39fd985575cd5edc560dfcf885da46f75819f
HkrOliver <118219630+HkrOliver@users.noreply.github.com>
2023-08-01 10:39:18 +0100

    tkinter Pt.2

--- PyAutomation.py
+++ PyAutomation.py
@@ -15,14 +15,32 @@ def Get_Path(button_pressed):
     if button_pressed=="destination_folder_Button":
         destination_folder=filedialog.askdirectory()
 
+def save_folders():
+    source_folder = source_folder_Textbox.get("1.0", "end-io")
+    destination_folder = destination_folder_Textbox.get("1.0", "end-io")
+    window.destroy()
 
+source_folder_Label = tk.Label(window, text="Source Folder:")
+source_folder_Label.grid(row=0, column=0, padx=10, pady=10)
 
+source_folder_Textbox = tk.Text(window, width=20, height=2)
+source_folder_Textbox.grid(row=0, column=1, padx=10, pady=10)
 
-source_folder_Label=tk.Label(window, text="Source Folder: "); source_folder_Label.pack(padx=1, pady=1)
-source_folder_Textbox=tk.Text(window); source_folder_Textbox.pack(padx=2, pady=2)
+source_folder_Browse = tk.Button(window, text="Browse", width=20, height=2)
+source_folder_Browse.grid(row=1, column=1, padx=50, pady=5)  # Adjust the 'pady' value
 
-destination_folder_Label=tk.Label(window, text="Destination Folder: "); destination_folder_Label.pack(padx=1, pady=1)
-destination_folder_Textbox=tk.Text(window); destination_folder_Textbox.pack(padx=2, pady=2)
+destination_folder_Label = tk.Label(window, text="Destination Folder:")
+destination_folder_Label.grid(row=2, column=0, padx=10, pady=10)  # Move to the next row
+
+destination_folder_Textbox = tk.Text(window, width=20, height=2)
+destination_folder_Textbox.grid(row=2, column=1, padx=10, pady=10)
+
+destination_folder_Browse = tk.Button(window, text="Browse", width=20, height=2)
+destination_folder_Browse.grid(row=3, column=1, padx=50, pady=5)  # Adjust the 'pady' value
+
+save_button = tk.Button(window, text="Save Folders", command=save_folders)
+save_button.grid(row=4, columnspan=2, padx=10, pady=10)
+
 
 
 window.mainloop()
